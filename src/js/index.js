b06278b83a9325cfc7bec1930436c8e95472926e
erxclau <ericlau41802@gmail.com>
2022-02-22 22:07:34 -0500

    Add eslint and husky

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
 window.onload = () => {
-  const node = document.getElementById("app");
+  const node = document.getElementById('app');
   render(<App />, node);
 };
 
-export default function App() {
-  return <>Hello, world!</>;
-}
+const App = () => {
+  console.log('hello, world');
+  return <>hello, world!</>;
+};
+
+export default App;
