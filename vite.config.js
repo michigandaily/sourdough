7eb100a53b1cfac6a2d7b0c98e6211f35cdd6588
erxclau <ericlau41802@gmail.com>
2022-07-03 13:59:20 -0500

    Use sass

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
 
-import nunjucks from './vite-plugin-transform-nunjucks.js';
+import nunjucks from './vite-plugin-transform-nunjucks';
 import hmrjson from './vite-plugin-hmr-json';
 
 // https://vitejs.dev/config/
