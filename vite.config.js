e174bdd911dbae4995f1f10ba33d7416672003eb
erxclau <ericlau41802@gmail.com>
2022-11-19 23:31:03 -0500

    Use external nunjucks plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dsv from '@rollup/plugin-dsv';
-
-import nunjucks from './vite-plugin-transform-nunjucks';
+import nunjucks from '@michigandaily/vite-plugin-transform-nunjucks';
 
 // https://vitejs.dev/config/
 export default defineConfig({
