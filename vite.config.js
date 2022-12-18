f516e8b6ef9bd091b2b11a4564dad44fe49c88a3
erxclau <ericlau41802@gmail.com>
2022-12-17 21:40:58 -0500

    Use internal DSV plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dsv from '@rollup/plugin-dsv';
-import nunjucks from '@michigandaily/vite-plugin-transform-nunjucks';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dsv from "@michigandaily/rollup-plugin-dsv";
+import nunjucks from "@michigandaily/vite-plugin-transform-nunjucks";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), dsv(), nunjucks()],
+  plugins: [
+    react(),
+    dsv({ include: ["**.csv", "**.tsv", "**.dsv"] }),
+    nunjucks(),
+  ],
 });
