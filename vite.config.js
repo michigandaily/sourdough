98e528903690607cf39d73f034149e4d5aecb7ee
Daniel Chuang <45211793+daniel-chuang@users.noreply.github.com>
2023-02-12 21:19:57 -0500

    Updated config.json to have a default key entry (#40)
    * Updated config.json to have a default key entry
    
    * added unspecified key to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dsv from "@michigandaily/rollup-plugin-dsv";
 import nunjucks from "@michigandaily/vite-plugin-transform-nunjucks";
+import { deployment } from "./config.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: deployment.key.length === 0 ? "/" : `/${deployment.key}/`,
   plugins: [
     react(),
     dsv({ include: ["**.csv", "**.tsv", "**.dsv"] }),
