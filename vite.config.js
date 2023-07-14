d1f65577c7bbd55f3633d52aa92a22d0bbfee99f
erxclau <ericlau41802@gmail.com>
2023-07-13 20:13:31 -0400

    Bump dependencies
    Closes #39

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dsv from "@michigandaily/rollup-plugin-dsv";
 import nunjucks from "@michigandaily/vite-plugin-transform-nunjucks";
-import { deployment } from "./config.json";
+import { deployment } from "./sink.config.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
