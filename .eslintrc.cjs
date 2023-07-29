fccd47b33e023d0746e05430e041bcaaa82fa962
erxclau <ericlau41802@gmail.com>
2023-07-29 12:45:09 -0400

    Adjust formatting and linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,19 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
-  ],
-  plugins: ["prettier"],
-  rules: {
-    "no-var": "error",
-    "prefer-const": "error",
-    "prefer-template": "error",
-    "template-curly-spacing": "error",
-    "prefer-arrow-callback": "error",
-    "arrow-spacing": "error",
-    eqeqeq: "error",
-    "prettier/prettier": "error",
-  },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'plugin:jsx-a11y/recommended',
+		'prettier'
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: { react: { version: '18.2' } },
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+	}
 };
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dsv from "@michigandaily/rollup-plugin-dsv";
-import nunjucks from "@michigandaily/vite-plugin-transform-nunjucks";
-import { deployment } from "./sink.config.json";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dsv from '@michigandaily/rollup-plugin-dsv';
+import nunjucks from '@michigandaily/vite-plugin-transform-nunjucks';
+
+import { deployment } from './sink.config.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: deployment.key.length === 0 ? "/" : `/${deployment.key}/`,
-  plugins: [
-    react(),
-    dsv({ include: ["**.csv", "**.tsv", "**.dsv"] }),
-    nunjucks(),
-  ],
+	base: deployment.key.length === 0 ? '/' : `/${deployment.key}/`,
+	plugins: [react(), dsv({ include: ['**.csv', '**.tsv', '**.dsv'] }), nunjucks()]
 });
